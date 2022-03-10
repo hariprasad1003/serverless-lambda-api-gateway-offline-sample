4104a5a21641015cc9451d14c58a87e28937a447
hariprasad1003 <hariprasadgurunathan10@gmail.com>
2022-03-10 17:17:52 +0530

    added post method

--- handler.py
+++ handler.py
@@ -1,10 +1,25 @@
 import json
 
-def hello(event, context):
+def get_greeting(event, context):
     try:
         return dict(
             statusCode=200,
             body="Hello World"
+        )
+    except Exception as e:
+        return dict(
+            statusCode=500,
+            body=str(e)
+        )
+
+def post_greeting(event, context):
+    try:
+
+        body = event["body"]
+
+        return dict(
+            statusCode=200,
+            body=f"{body}"
         )
     except Exception as e:
         return dict(
